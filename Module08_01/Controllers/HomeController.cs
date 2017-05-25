e7ecaa0fa72fff112112972527890ca463ebea42
auxo86 <auxo86@gmail.com>
2017-05-25 19:09:01 +0800

    08-02

---
+++ Module08_01/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module08_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get:Home/HelloWorld
+        //PartialView不能使用layout
+        public ActionResult HelloWorld()
+        {
+            ViewBag.message = "HelloWorld:"+ DateTime.Now.ToString();
+            return PartialView();
+        }
+
+        public ActionResult HelloWorld2(string username)
+        {
+            ViewBag.message = "HelloWorld:" + username + ", " + DateTime.Now.ToString();
+            return PartialView("HelloWorld");
+        }
+
+    }
+}
---
+++ Module08_01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Module08_01.MvcApplication" Language="C#" %>
